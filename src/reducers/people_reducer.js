1d1f818359d5df97fa36d4f7d28c4b73cce9a480
AdamT213 <adt6261@gmail.com>
2018-04-09 15:55:27 -0400

    installed react redux separately

--- src/reducers/people_reducer.js
+++ src/reducers/people_reducer.js
@@ -1,5 +1,5 @@
 
-export function peopleReducer(state = {loading: false, person: {}, action}) {
+export function peopleReducer(state = {loading: false, person: {}}, action) {
   switch (action.type) {
 
      case 'GET_PERSON': 
@@ -11,7 +11,7 @@ export function peopleReducer(state = {loading: false, person: {}, action}) {
       return state;
      }
       case 'ADD_PERSON': 
-      let currentPerson = action.payload 
+        currentPerson = action.payload 
        return {loading: false, person: currentPerson}; 
      default:
        return state; 
