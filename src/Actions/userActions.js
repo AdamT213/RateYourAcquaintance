1d29b5f4295e24f6020c58daa58033efb9f600b9
AdamT213 <adt6261@gmail.com>
2018-05-03 18:01:43 -0400

    added one-time terms and conditions alert on sign-up

--- src/Actions/userActions.js
+++ src/Actions/userActions.js
@@ -29,7 +29,10 @@ export function signUpUser(user){
         }).then(responseJson => { 
           console.log(responseJson) 
           //call action to set current user and store token
-          dispatch({type: 'SET_TOKEN', payload: responseJson})
+          dispatch({type: 'SET_TOKEN', payload: responseJson}) 
+      }).then(res=>{ 
+        //for now, having terms and conditons/opening remarks pop up as a 1-time alert on signup
+        alert("Terms and Conditions: We, the creators of RateYourAcquaintance, recognize the potential for this product to cause great harm and negatively impact lives. However, when used properly, we believe it can also be a tool for empowerment, used to bring others up. This is an application for mature adults, and should be used as such. Hate speech, libel and the spreading of rumors will not be tolerated. Negative reviews must reference a   SPECIFIC ENCOUNTER or else they will be removed at the moderator's discretion. If you must leave a negative review, try to maintain the Karmic balance by leaving a positive review for somebody else. Above all, remember, the way you talk about others speaks volumes about you. By proceeding, you acknowledge these terms.")
       })
     })
   } 
