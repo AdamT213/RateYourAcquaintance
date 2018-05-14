f2920b557e20edea2be7d6f40081c204cbc3b5f5
AdamT213 <adt6261@gmail.com>
2018-05-14 02:31:01 -0400

    made showing average star rating as a float rounded off to two decimal places work

--- src/Presentational/FormattedPerson.js
+++ src/Presentational/FormattedPerson.js
@@ -7,14 +7,16 @@ export class FormattedPerson extends Component {
   render() { 
 
     const reviews =  this.props.reviews.map((review, index) => {
-    return <Review star_rating={review.star_rating} description={review.description} key={index} />}); 
+    return <Review star_rating={review.star_rating} description={review.description} key={index} />});  
 
+      // function to find average star rating for a person, rounded off to two digits. Star ratings are still not saving as floats however, or at least not as fractional ratings
     var average_star_rating = function(){ 
-      let totalStars = 0
-      this.props.reviews.forEach((review) => { 
-        totalStars += review.star_rating 
+      debugger;
+      let totalStars = 0.0
+      reviews.forEach((review) => { 
+        totalStars += review.props.star_rating 
       }) 
-      return totalStars/this.props.reviews.length
+      return (totalStars/parseFloat(reviews.length)).toFixed(2)
     }
     
       return( 
@@ -22,8 +24,8 @@ export class FormattedPerson extends Component {
           <p>Name: {this.props.name}</p><br />
           <p>Description: {this.props.description}</p><br /> 
           <p>Location: {this.props.location}</p><br />  
-          <p>Average Star Rating: <Rater total={5} rating={average_star_rating}
-          interactive={false}/> average_star_rating</p>
+          <p>Average Star Rating: <Rater total={5} rating={average_star_rating()}
+          interactive={false}/> {average_star_rating()}</p>
           <ul> 
             {reviews} 
           </ul><br /> 
